162cd278c3465362268393475a4d515e2c30de48
zongjie233 <840573195@qq.com>
2020-05-10 14:19:18 +0800

    5.9

--- 列表生成式.py
+++ 列表生成式.py
@@ -1,2 +1,43 @@
 #列表生成式即List Comprehensions 是python中内置的 用来创建list生成式
-test1
+print(list(range(1,10)))
+
+
+l1 = [x * x for x in range(1,10)]
+print(l1)
+
+#仅选出偶数的平方
+l2 = [x * x for x in range(1,5) if x % 2 == 0]
+print(l2)
+
+#两层循环
+l3 = [m + n for m in 'huang' for n in 'shang']
+print(l3)
+
+import os
+#listdir 列出文件和目录
+print([d for d in os.listdir('.')])
+
+#同时迭代字典中的key和value
+d = {
+    'one' : '1',
+    'two' : '2',
+    'three' : '3'
+    }
+for k, v in d.items():
+    print(f'{k} = {v}')
+
+#使用两个变量生成list
+print([k + '=' + v for k,v in d.items()])
+
+#所有字符串变小写
+L = ['HUANG','SHANG']
+print([s.lower() for s in L])
+
+#可见，在一个列表生成式中，for前面的if ... else是表达式，而for后面的if是过滤条件，不能带else。
+
+#ex  修改列表生成式，通过添加if语句保证列表生成式正确执行
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s,str)]
+print(L1)
+print(L2)
+
---
+++ 生成器.py
@@ -0,0 +1,71 @@
+#generator
+#创建方法1
+L = [x * x for x in range(10)]
+print(L)
+#将[]改为()即可
+g = (x * x for x in range(10))
+print(g)
+
+#通过next()函数来获取generator的下一个返回值,但正确方法应该是使用for循环
+for n in g:
+    print(n)
+
+#输出斐波拉契数列前n项
+def fib(max):
+    n,a,b = 0,0,1
+    while n < max:
+        print(b)
+        a, b = b, a+b
+        n += 1
+    return 'done'
+
+fib(10)
+
+#如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通的函数，而是一个generator
+def fib_generator(max):
+    n,a,b = 0,0,1
+    while n < max:
+        yield b
+        a, b = b, a+b
+        n += 1
+    return 'done'
+f = fib_generator(10)
+print(f)
+#变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+
+def odd():
+    print('step1')
+    yield
+    print('step2')
+    yield
+    print('step3')
+    yield
+
+#调用generator时，首先要生成一个generator对象，然后用next()函数获取下一个返回值
+a = odd()
+next(a)
+next(a)
+next(a)
+
+#用for循环调用generator时，拿不到return语句中的返回值，如果想要拿回返回值必须不过StopIteration
+g = fib_generator(6)
+while True:
+    try:
+        x = next(g)
+        print('g:',x)
+    except StopIteration as e:
+        print('return value:', e.value)
+        break
+
+#杨辉三角形
+def triangles(max):
+    n = 0
+    l1 = [1]
+    while n < max:
+        yield l1
+
+
+
+
+
+
